6cbf7eaeff176318662c5ed772507c0f6e90bf8c
A.Bond <alexey.bond.94.55@gmail.com>
2017-12-03 00:07:44 +0600

    Add ui skins. And ui.

---
+++ android/assets/skins/neon/README.md
@@ -0,0 +1,1 @@
+[Source](https://ray3k.wordpress.com/neon-ui-skin-for-libgdx/).
--- core/src/com/github/oldnpluslusteam/old40_game/screens/StartupScreen.java
+++ core/src/com/github/oldnpluslusteam/old40_game/screens/StartupScreen.java
@@ -2,11 +2,13 @@ package com.github.oldnpluslusteam.old40_game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.github.alexeybond.partly_solid_bicycle.application.Layer;
 import com.github.alexeybond.partly_solid_bicycle.application.Screen;
 import com.github.alexeybond.partly_solid_bicycle.application.impl.DefaultScreen;
 import com.github.alexeybond.partly_solid_bicycle.application.impl.layers.GameLayer;
 import com.github.alexeybond.partly_solid_bicycle.application.impl.layers.GameLayerWith2DPhysicalGame;
+import com.github.alexeybond.partly_solid_bicycle.application.impl.layers.StageLayer;
 import com.github.alexeybond.partly_solid_bicycle.application.util.ScreenUtils;
 import com.github.alexeybond.partly_solid_bicycle.drawing.Technique;
 import com.github.alexeybond.partly_solid_bicycle.drawing.tech.EDSLTechnique;
@@ -32,7 +34,8 @@ public class StartupScreen extends DefaultScreen {
                         pass("game-background"),
                         pass("game-light"),
                         pass("game-objects"),
-                        pass("game-debug")
+                        pass("game-debug"),
+                        pass("ui-main")
                 );
             }
         };
@@ -44,8 +47,10 @@ public class StartupScreen extends DefaultScreen {
 
         ScreenUtils.enableToggleDebug(this, true);
 
-        GameLayer gameLayer = new GameLayerWith2DPhysicalGame();
-        layers.add("game", gameLayer);
+        GameLayer gameLayer = layers.add("game", new GameLayerWith2DPhysicalGame());
+        StageLayer stageLayer = layers.add("ui", new StageLayer("ui-main"));
+
+        // game setup
 
         gameLayer.game().systems().add("light", new LightingSystemImpl());
 
@@ -54,9 +59,30 @@ public class StartupScreen extends DefaultScreen {
 
         GameDeclaration gameDeclaration = IoC.resolve(
                 "load game declaration",
-                Gdx.files.internal("level0.json"));
+                Gdx.files.internal("level4.json"));
 
         new ApplyGameDeclarationVisitor().doVisit(gameDeclaration, gameLayer.game());
+
+        //
+
+        Skin skin = IoC.resolve("load skin", "skins/neon/neon-ui.json");
+
+        final Label progressLabel = new Label("", skin);
+        final ProgressBar progressBar = new ProgressBar(0, 1, 1, false, skin);
+        final Button button = new TextButton("Next", skin);
+
+        button.setDisabled(true);
+        button.setVisible(false);
+
+        VerticalGroup group = new VerticalGroup();
+        group.addActor(progressLabel);
+        group.addActor(progressBar);
+        group.addActor(button);
+        group.setFillParent(true);
+
+        stageLayer.stage().addActor(group);
+
+        // wincondition setup
 
         final IntProperty wc = gameLayer.game().events().event("winCondition", IntProperty.make());
         final IntProperty wd = gameLayer.game().events().event("winDone", IntProperty.make());
@@ -64,11 +90,17 @@ public class StartupScreen extends DefaultScreen {
         EventListener<IntProperty> winListener = new EventListener<IntProperty>() {
             @Override
             public boolean onTriggered(IntProperty event) {
-                Gdx.graphics.setTitle(""+wd.get()+"/"+wc.get()+" done");
+                progressLabel.setText("Done " + wd.get() + " of " + wc.get());
+                progressBar.setRange(0, wc.get());
+                progressBar.setValue(wd.get());
                 if (wd.get() != 0 && wd.get() == wc.get()) {
                     // win
+                    button.setDisabled(false);
+                    button.setVisible(true);
                 } else {
                     // no win
+                    button.setDisabled(true);
+                    button.setVisible(false);
                 }
                 return false;
             }
@@ -76,6 +108,7 @@ public class StartupScreen extends DefaultScreen {
 
         wc.subscribe(winListener);
         wd.subscribe(winListener);
+        wd.trigger();
 
         Gdx.gl.glClearColor(0.2f, 0.2f, 0.24f, 0.0f);
     }
